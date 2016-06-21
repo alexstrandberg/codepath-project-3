19ca3196d02e6d7f711cd8e34fd8f123a24adcf3
Alexander Strandberg <alexstrandberg@fb.com>
2016-06-21 16:00:06 -0700

    Username, creation time showing

---
+++ Parsetagram/FeedHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  FeedHeaderView.swift
+//  Parsetagram
+//
+//  Created by Alexander Strandberg on 6/21/16.
+//  Copyright © 2016 Alexander Strandberg. All rights reserved.
+//
+
+import UIKit
+
+class FeedHeaderView: UITableViewCell {
+    @IBOutlet weak var usernameLabel: UILabel!
+
+    @IBOutlet weak var timestampLabel: UILabel!
+}
